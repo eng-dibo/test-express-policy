2db868b446b5d7ebcebe656e77251604e1cf5316
dibo <sherif.eldeeb@learnovia.com>
2022-12-13 10:12:53 +0200

    CSP

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 let app = express();
 
-app.use("*", (req: Request, res: Response) => res.send("OK"));
+// set CSP
+app.use((req: Request, res: Response) =>
+  res.set("Content-Security-Policy", "default-src 'self'; content-src 'self'")
+);
+
+app.use("/ok", (req: Request, res: Response) => res.send("ok"));
+
+app.use("*", (req: Request, res: Response, next: NextFunction) =>
+  next("error")
+);
 
 app.listen(5000, () => console.log("listening..."));
 
-// fetch(`http://localhost:5000`)
+// // fetch:
+// fetch(`http://localhost:5000/ok`);
+
+// // ajax:
+// let oReq = new XMLHttpRequest();
+// oReq.open("get", "http://localhost:5000/ok", true);
+// oReq.send();
+// console.log(oReq.response);
